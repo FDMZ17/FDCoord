5e851efa5a0c5dfba05138706cc7fb18186cbf04
FDMZ17 <developer@fdmz17.eu.org>
2022-11-30 14:24:27 +0700

    Changes!

--- pages/api/ws.ts
+++ pages/api/ws.ts
@@ -1,45 +1,38 @@
 import { Server } from "socket.io";
 
 
-export default function handler(req:any ,res:any) {
+export default function handler(req: any, res: any) {
   // It means that socket server was already initialised
   if (res.socket.server.io) {
-    console.log("Already set up");
+    console.info("Already set up");
     res.end()
     return;
   }
 
   const io = new Server(res.socket.server);
   res.socket.server.io = io;
-  const USERS = [];
 
   // socket io on connected user
-  io.on("connection", (socket:any) => {
-    console.log(socket.id)
-    // disconnected user
+  io.on("connection", (socket: any) => {
     socket.on("disconnect", () => {
-      console.log(socket.id + " user diconnected");
     });
 
     // listen message
-    socket.on("message", (message:any) => {
-      message.date = new Date().toLocaleString();
-      console.log(message);
+    socket.on("message", (message: any) => {
       io.emit("message", message);
     });
 
     // is typing
-    socket.on("typing", (user:string) => {
-      console.log(`${user} is typing...`);
+    socket.on("typing", (user: string) => {
       io.emit("typing", user);
     });
 
     // stop typing
-    socket.on("stop-typing", (user:string) => {
+    socket.on("stop-typing", (user: string) => {
       io.emit("stop-typing", user);
     });
   });
 
-  console.log("Setting up socket");
+  console.info("Setting up socket");
   res.end()
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
 import Head from "next/head";
-import Image from "next/image";
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
 const {
     uniqueNamesGenerator,
-    adjectives,
-    colors,
-    animals,
+    starWars,
+
 } = require("unique-names-generator");
 const randomName = uniqueNamesGenerator({
-    dictionaries: [adjectives, colors, animals],
+    dictionaries: [starWars],
 });
 
 fetch('http://localhost:3000/api/ws')
@@ -23,73 +21,88 @@ export default function Home() {
 
     useEffect(() => {
         socket.on("connect", () => {
-            console.log("connect");
+            console.info("connected");
         });
         socket.on("disconnect", () => {
-            console.log("disconnect");
+            console.info("disconnect");
         });
         socket.on("message", (val) => {
             setWhoIsTyping("");
-            setMessages([...messages, val]);
+            setMessages([...messages, val])
         });
-
+        socket.on("newUser", (val) => {
+            setMessages([...messages, val])
+        });
         socket.on("typing", (val) => {
-            if (val != nickname) setWhoIsTyping(`${val} is typing...`);
+            if (val != nickname) setWhoIsTyping(`| ${val} is typing...`);
         });
-
         socket.on("stop-typing", () => {
             setWhoIsTyping("");
         });
-
         return () => {
             socket.off("connect");
             socket.off("disconnect");
             socket.off("message");
             socket.off("typing");
+            socket.off("newUser")
         };
     });
+    // generate ID
+    function gen(length: number) {
+        let result = [];
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        for (let i = 0; i < length; i++) {
+            result.push(characters.charAt(Math.floor(Math.random() *
+                charactersLength)));
+        }
+        return result.join('');
+    }
 
     // function emit message
     const sendMessage = (e: any) => {
         e.preventDefault();
+        if (!inputText) return
         socket.emit("message", {
             from: nickname,
             message: inputText,
+            id: gen(16)
         });
         socket.emit("stop-typing", nickname);
         setInputText("");
     };
+
     return (
         <>
             <Head>
-                <title>FDCoord - Chat app</title>
-                <meta name="description" content="FDCoord - Chat App" />
+                <title>FDCord - Chat app</title>
+                <meta name="description" content="FDCord - Chat App" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className="bg-zinc-800 w-screen h-screen">
+            <main className="w-screen h-screen bg-zinc-800">
                 {/* header */}
                 <header id="topNavGroupName">
-                    <div className="flex fixed top-0 flex-col pt-1 m-0 w-screen md:h-18 h-26 mb-26 md:mb-18 shadow-lg bg-zinc-900">
+                    <div className="flex fixed top-0 flex-col pt-1 m-0 w-screen shadow-lg md:h-18 h-26 mb-26 md:mb-18 bg-zinc-900">
                         <div className="ml-24 lg:ml-28">
-                            <p className="text-lg font-bold text-white">FDCood | {whoIsTyping}</p>
+                            <p className="text-lg font-bold text-white">FDCord {whoIsTyping}</p>
                             <label className="label">Your Nickname: </label>
                             <input
                                 type="text"
-                                className="bg-zinc-600 mx-2 my-2 rounded-sm outline-none py-1 text-center"
+                                className="py-1 mx-2 my-2 text-center rounded-sm outline-none bg-zinc-600"
                                 value={nickname}
                                 onChange={(e) => setNickname(e.target.value)}
                             />
                         </div>
                     </div>
                 </header>
-                <div className="rounded p-2 scroll bg-zinc-800 pb-16 pt-32 flex-row">
+                <div className="overflow-scroll p-2 pt-32 pb-16 rounded bg-zinc-800">
                     {/* message content */}
                     {messages.map((e: any, index: any) => (
                         <div
                             className={
                                 e.from == nickname
-                                    ? "p-2 text-end border rounded mb-2 w-auto"
-                                    : "p-2 text-start border rounded mb-2 w-auto"
+                                    ? "p-2 text-end rounded mb-2 w-auto"
+                                    : "p-2 text-start rounded mb-2 w-auto"
                             }
                             key={index}
                         >
@@ -97,8 +110,6 @@ export default function Home() {
                                 <strong>{e.from == nickname ? "You" : e.from}</strong>{" "}
                                 <br></br>
                                 {e.message}
-                                <br></br>
-                                {e.date}
                             </p>
                         </div>
                     ))}
@@ -106,9 +117,9 @@ export default function Home() {
                 </div>
                 <div id="chatBar">
                     <form onSubmit={sendMessage}>
-                        <div className="flex fixed bottom-1 right-1 rounded-lg">
-                            <input id="messageBox" type="text" className="flex outline-none border-none px-3 md:w-[192vh] w-[38vh] text-white bg-zinc-900" size={50}
-                                placeholder="Message ${group.Name}" // ={sendMessage}
+                        <div className="flex fixed right-1 bottom-1 rounded-lg">
+                            <input id="messageBox" type="text" className="flex outline-none border-none rounded-sm px-3 md:py-2 md:w-screen mb-2 w-[42vh] text-white bg-zinc-900" size={100}
+                                placeholder="Type a message"
                                 required
                                 value={inputText}
                                 onChange={(e) => {
@@ -122,8 +133,8 @@ export default function Home() {
                                     }, 4000);
                                 }}
                             />
-                            <span className="p-2 text-3xl border-r-4 bg-zinc-700 border-zinc-700">
-                                <svg onClick={sendMessage} className="block w-7 h-7 rounded-xl bottom-0 cursor-pointer bg-white hover:bg-white"
+                            <span className="p-2 text-3xl border-r-4 md:hidden bg-zinc-700 border-zinc-700">
+                                <svg onClick={sendMessage} className="block bottom-0 w-7 h-7 bg-white rounded-xl cursor-pointer hover:bg-white"
                                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
                                 </svg>
